2c6a8fe0db6bde3ea21a17ade6ca021630c8c6d9
jorge <jorgemartinez797@yahoo.com>
2024-08-20 23:03:44 -0600

    Tried making blackjack using classes

--- blackjack.py
+++ blackjack.py
@@ -1,55 +1,48 @@
 import random
 from time import sleep
 
-
 symbols_list = ("Clubs (♣)","Diamonds (♦)","Hearts (♥)","Spades (♠)")
 values_list = (1,2,3,4,5,6,7,8,9,10,"King","Queen","Ace")
 
 
-#Classes
-class Game:
-    #Sets up deck and players
-    def __init__(self): 
-        self.deck = Deck()
-        self.player = Player("Player")
-        self.dealer = Player("Dealer")
-
-    #Handles flow of game, turns, actions, winning conditions like blackjack and bust
-    def start_game(self): 
-        ...
-
-
-class Player:
-    #Store and manage players hand, calculates value of hands, player actions like hit stand and double down
-    def __init__(self, name): 
-        self.name = name
-        self.hand = []
-        self.value = 0
-        self.double_down = False
-        
-    def draw_card(self):
-        self.card = Deck.deal_card()
-
-    def update_hand_value(self):
-        ...
-
 class Deck:
-    #Creating and shuffling the deck, provides card to players, no repeated cards
     def __init__(self): 
-        self.cards = [Card(value, suit) for suit in symbols_list for value in values_list]
+        #Creates a list of CARD objects for each combination of cards and shuffles them
+        self.cards = [Card(value,suite) for value in values_list for suite in symbols_list]
         random.shuffle(self.cards)
 
-    def deal_card(self): 
-        ...
+    def draw_card(self):
+        #Draws ONE card by calling on the POP function, pop function deletes and returns the last item in the list 
+        return self.cards.pop()
 
 
 class Card:
-    def __init__(self, value, suit): 
+    def __init__(self, value, suite):
         self.value = value
-        self.suit = suit
+        self.suite = suite
+    
 
-    def determine_value(self): 
+    def __str__(self) -> str:
+        return f"{self.value} of {self.suite} "
+    
+class Player:
+    def __init__(self,name): 
+        self.name = name
+        self.hand = []
+        self.value = 0
+        self.can_doubledown = False
+
+    #Draws a card and updates value
+    def draw(self): 
         ...
 
-    def __str__(self):
-        return f"{self.value} of {self.suit}"
+class Game:
+    def __init__(self) -> None:
+        player = Player(input("Player name: "))
+        dealer = Player("Dealer")
+        deck = Deck()
+
+#TODO Implement draw feature 
+
+
+s = Game()
---
+++ unit_converter.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+myLabel = tk.Label(root, text="Hello World")
+
+myLabel.pack(padx=10,pady=10)
+
+root.mainloop()
+
