ba54013f080b6ef6479ae1c427753f2fffff59b6
jorge <jorgemartinez797@yahoo.com>
2024-08-25 11:02:39 -0600

    Linking classes

--- blackjack.py
+++ blackjack.py
@@ -14,35 +14,59 @@ class Deck:
     def draw_card(self):
         #Draws ONE card by calling on the POP function, pop function deletes and returns the last item in the list 
         return self.cards.pop()
+    
+    def cards_remaining(self): 
+        return len(self.cards)
 
 
 class Card:
     def __init__(self, value, suite):
         self.value = value
         self.suite = suite
-    
+
+    def card_value(self):
+        if self.value == "Ace": 
+            return 11
+        if self.value == "King" or self.value == "Queen": 
+            return 10
+        return self.value
 
     def __str__(self) -> str:
         return f"{self.value} of {self.suite} "
     
 class Player:
-    def __init__(self,name): 
-        self.name = name
+    def __init__(self): 
         self.hand = []
         self.value = 0
         self.can_doubledown = False
 
-    #Draws a card and updates value
-    def draw(self): 
-        ...
+    #Draws a card, returns a card CLASS, appends it to the hand, and updates value
+    def draw(self, deck): 
+        self.card = deck.draw_card()
+        self.hand.append(self.card)
+        print(f"You got the {self.card}")
+        self.hand_value()
+
+    def hand_value(self): 
+        for card in self.hand: 
+            x = card.card_value 
+            
+
+
 
 class Game:
-    def __init__(self) -> None:
-        player = Player(input("Player name: "))
-        dealer = Player("Dealer")
-        deck = Deck()
-
-#TODO Implement draw feature 
+    def __init__(self): 
+        self.player = Player()
+        self.dealer = Player()
+        self.deck = Deck()
 
 
-s = Game()
+def main(): 
+    game = Game()
+    game.player.draw(game.deck)
+
+
+
+
+if __name__ == "__main__": 
+    main()
